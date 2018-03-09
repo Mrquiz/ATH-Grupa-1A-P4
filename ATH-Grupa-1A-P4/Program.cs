e7a309d4bdf0d931068ef1afc5a163c0106ba9a3
Maciej <Clinkz155@gmail.com>
2018-03-09 12:08:53 +0100

    init Zadanie2

--- ATH-Grupa-1A-P4/Program.cs
+++ ATH-Grupa-1A-P4/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab03a
+namespace ATH_Grupa_1A_P4
 {
     class Program
     {
@@ -15,20 +15,7 @@ namespace Lab03a
 
                 context.Database.Log = Console.WriteLine;
 
-                var top5CustomersWithEmployee = context.Customers
-                    .OrderBy(x => x.CompanyName)
-                    .Take(5)
-                    .Select(x => new
-                    {
-                        x.CompanyName,
-                        Orders = x.Orders.Select(y => new
-                        {
-                            OrderId = y.OrderID,
-                            EmloyeeLastName = y.Employees.LastName
-                        })
-                    });
-
-                var ordersPrice = context.Orders
+                var currentQuery = context.Orders
                     .Select(x => new
                     {
                         x.OrderID,
@@ -36,52 +23,12 @@ namespace Lab03a
                         Price = x.Order_Details.Sum(y => y.Quantity * y.UnitPrice)
                     });
 
-                var employeeWithOrderPriceAndTerritory = context.Employees
-                    .Select(x => new
-                    {
-                        Name = x.FirstName + " " + x.LastName,
-                        OrderPrice = x.Orders
-                            .Sum(y => y.Order_Details.Sum(z => z.UnitPrice * z.Quantity)),
-                        Territories = x.Territories.Select(y => y.TerritoryDescription)
-                    })
-                    .OrderByDescending(x => x.OrderPrice);
-
-                var hierarchy = context.Employees
-                    .Where(x => x.Boss == null);
-
-                var top10ProductsWithBiggestDiscount = context.Order_Details
-                    .OrderByDescending(x => x.Discount)
-                    .Take(10)
-                    .Select(x => new
-                    {
-                        x.Products.ProductID,
-                        x.Products.ProductName,
-                        x.Discount
-                    });
-
-
-
-                foreach (var query in top10ProductsWithBiggestDiscount)
+                foreach (var query in currentQuery)
                 {
-                    //Console.WriteLine(customer.CompanyName);
-                    //foreach (var customerOrder in customer.Orders)
-                    //{
-                    //    Console.WriteLine($" - {customerOrder.OrderId} {customerOrder.EmloyeeLastName}");
-                    //}
                     Console.WriteLine(query);
 
                 }
             }
-        }
-
-        static void ShowEmployee(Employees emplyoee, int level = 0)
-        {
-            Console.WriteLine("{0} - {1}", "".PadLeft(level * 2, ' '), emplyoee.LastName);
-            foreach (var emp in emplyoee.Employees1)
-            {
-                ShowEmployee(emp, level + 1);
-            }
-
         }
     }
 }
