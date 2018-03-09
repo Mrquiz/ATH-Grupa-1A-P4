e72c55b8e48a67bb118573bf2e14f06b32e4aa22
Maciej <Clinkz155@gmail.com>
2018-03-09 11:44:31 +0100

    init Visual Studio Project C#

---
+++ ATH-Grupa-1A-P4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATH_Grupa_1A_P4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
